47c6dd0602a10232e41fbd2be90a714dc4d137f1
namphuong110401 <114762617+namphuong110401@users.noreply.github.com>
2022-10-07 09:39:40 -0400

    Delete AngryBee.java

--- AngryBee.java
+++
@@ -1,8 +0,0 @@
-public class AngryBee extends HoneyBee{
-    private int attackDamage;
-
-    public AngryBee(Tile t, int attackDamage) {
-        super(t, 10, 1);
-        this.attackDamage = attackDamage;
-    }
-}
